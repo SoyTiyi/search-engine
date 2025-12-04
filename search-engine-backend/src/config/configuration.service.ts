cb7c3ccb16ec434012af076f808c28bf0b14ee30
SoyTiyi <santiago.marmartinez@gmail.com>
2025-12-04 16:56:34 -0500

    add interface for config

--- search-engine-backend/src/config/configuration.service.ts
+++ search-engine-backend/src/config/configuration.service.ts
@@ -1,35 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService as NestConfigService } from '@nestjs/config';
+import {
+  ServerConfig,
+  AmadeusConfig,
+  FrontendConfig,
+} from './intefaces/config.interface';
 
 @Injectable()
 export class ConfigurationService {
   constructor(private configService: NestConfigService) {}
 
-  get server(): { port: string } {
+  get server(): ServerConfig {
     return {
-        port: this.configService.get<string>('server.port') || '',
+      port: this.configService.get<number>('server.port') || 0,
     };
   }
 
-  get amadeus(): {
-    apiKey: string;
-    apiSecret: string;
-    baseUrl: string;
-    authBaseUrl: string;
-    timeout: number;
-  } {
+  get amadeus(): AmadeusConfig {
     return {
-        apiKey: this.configService.get<string>('amadeus.apiKey') || '',
-        apiSecret: this.configService.get<string>('amadeus.apiSecret') || '',
-        baseUrl: this.configService.get<string>('amadeus.baseUrl') || '',
-        authBaseUrl: this.configService.get<string>('amadeus.authBaseUrl') || '',
-        timeout: this.configService.get<number>('amadeus.timeout') || 0,
+      apiKey: this.configService.get<string>('amadeus.apiKey') || '',
+      apiSecret: this.configService.get<string>('amadeus.apiSecret') || '',
+      baseUrl: this.configService.get<string>('amadeus.baseUrl') || '',
+      authBaseUrl: this.configService.get<string>('amadeus.authBaseUrl') || '',
+      timeout: this.configService.get<number>('amadeus.timeout') || 0,
     };
   }
 
-  get frontend(): { url: string } {
+  get frontend(): FrontendConfig {
     return {
-        url: this.configService.get<string>('frontend.url') || '',
+      url: this.configService.get<string>('frontend.url') || '',
     };
   }
 }
