9de9be13a2edf5297f32a791f7fc61774c7c43ef
SoyTiyi <santiago.marmartinez@gmail.com>
2025-12-04 17:07:11 -0500

    remove old auth controller

--- search-engine-backend/src/app.module.ts
+++ search-engine-backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { FlightsController } from './flights/flights.controller';
 import { FlightsModule } from './flights/flights.module';
+import { ConfigurationModule } from './config/configutation.module';
 
 @Module({
-  imports: [AuthModule, FlightsModule],
+  imports: [AuthModule, FlightsModule, ConfigurationModule],
   controllers: [AppController, FlightsController],
   providers: [AppService],
 })
