7f43de84d180b1f9a120d333c2224c9ce9572ec8
SoyTiyi <santiago.marmartinez@gmail.com>
2025-12-06 11:24:52 -0500

    use AllExceptionsFilter class and also config for Swagger, Cors and Pipes for class validations

--- search-engine-backend/src/main.ts
+++ search-engine-backend/src/main.ts
@@ -1,8 +1,46 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { AllExceptionsFilter } from './common/filters/http-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  app.useGlobalFilters(new AllExceptionsFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('Flight Search API')
+    .setDescription('API para buscar vuelos usando Amadeus')
+    .setVersion('1.0')
+    .addTag('flights')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+
+  console.log(`
+    Server running on: http://localhost:${port}
+    Swagger docs: http://localhost:${port}/api/docs
+  `);
 }
 bootstrap();
