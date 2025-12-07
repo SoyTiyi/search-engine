69f4b2cdec13fc9b5ea30c5aedc26f023e50ba07
Santiago Martínez Martínez <46984191+SoyTiyi@users.noreply.github.com>
2025-12-06 19:19:29 -0500

    install and configute nest throttlet module on app module for rate limit (#18)

--- search-engine-backend/src/app.module.ts
+++ search-engine-backend/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -9,6 +10,7 @@ import { AmadeusModule } from './amadeus/amadeus.module';
 import { CacheModule } from './cache/cache.module';
 import { SearchHistory } from './flights/entities/search-history.entity';
 import { HealthModule } from './health/health.module';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 
 @Module({
   imports: [
@@ -18,6 +20,14 @@ import { HealthModule } from './health/health.module';
       entities: [SearchHistory],
       synchronize: true,
     }),
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 10,
+        },
+      ],
+    }),
     AuthModule,
     FlightsModule,
     ConfigurationModule,
@@ -26,6 +36,12 @@ import { HealthModule } from './health/health.module';
     HealthModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
